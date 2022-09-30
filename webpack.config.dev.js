f3cc8390d8d63a347b3f271732ba0917f34f74bd
Kmero <alejandrocamero01@gmail.com>
2022-09-30 10:57:12 -0400

    actualizacion con webpack devtools

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require ('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const DotEnv = require ('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;;
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -11,7 +12,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
+    devtool: 'source-map',
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -66,8 +67,14 @@ plugins: [
             }
         ]
     }),
-    new DotEnv({
-
-        }),
+    new DotEnv(),
+    new BundleAnalyzerPlugin (),
 ],
+devServer: {
+    static:path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+},
+
 }
